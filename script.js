11f77235968d5470ee3aa5dea83aae05dd09dd36
jorgeg8r-bit <jorgeg8r@gmail.com>
2026-02-02 20:21:42 -0600

    Replace English messages with Spanish alternatives

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Sofi Estas Segura?",
+    "Muy segura??",
+    "Muy Muy Muy Muy Muy segura?",
+    "Soy Gracioso de repente",
+    "Minimo Piensalo!",
+    "Si Dices que no se muere bts...",
+    "Chofiii piensalo bien...",
+    "Te pago por favor...",
+    "Okey,ya no insistire...",
+    "Broma, solo di que siii😩😭! ❤️"
 ];
 
 let messageIndex = 0;
